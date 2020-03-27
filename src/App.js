7ee1b2ad1ec2457f0a10c3767c5dee734d90328b
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-03-27 14:31:15 +0200

    remove unused images

--- src/App.js
+++ src/App.js
@@ -8,38 +8,6 @@ import store from './store';
 import Root from './components/Root';
 
 class App extends Component {
-  // state = {
-  //   languages: [],
-  //   paymentTypes: [],
-  //   cartItems: []
-  // };
-
-  // addToCartHandler = (product, quantity = 1) => {
-  //   const cartItems = [...this.state.cartItems];
-  //   let index = cartItems.findIndex(item => item.product.id === product.id);
-  //   if (index !== -1) {
-  //     cartItems[index] = { ...cartItems[index] };
-  //     cartItems[index].count += quantity;
-  //   } else {
-  //     cartItems.push({ product: { ...product }, count: quantity });
-  //   }
-  //   this.setState({ cartItems });
-  // };
-
-  // removeItemHandler = item => {
-  //   const cartItems = this.state.cartItems.filter(
-  //     cartItem => item.product.id !== cartItem.product.id
-  //   );
-  //   this.setState({ cartItems });
-  // };
-
-  // editProductHandler = product => {
-  //   const products = [...this.state.products];
-  //   const index = products.findIndex(p => p.id === product.id);
-  //   products[index] = product;
-  //   this.setState({ products });
-  // };
-
   render() {
     return (
       <Provider store={store}>
